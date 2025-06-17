2a455170c77da601198c552e0f67e35233fd786a
Anuj Gupta <anujgupta@Anujs-MacBook-Air.local>
2025-06-17 18:04:12 +0530

    dabaase trying again

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const pool = require('./db/connect')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
